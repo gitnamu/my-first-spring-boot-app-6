d6d49b23f00ede416c4b9dad7244d6dd89db1307
Kim Namho <skagh1750@gmail.com>
2021-04-04 14:37:25 +0900

    Initial commit

---
+++ src/main/java/org/cnu/realcoding/myfirstspringbootapp6/MyFirstSpringBootApp6Application.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding.myfirstspringbootapp6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringBootApp6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringBootApp6Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/cnu/realcoding/myfirstspringbootapp6/MyFirstSpringBootApp6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding.myfirstspringbootapp6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringBootApp6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
