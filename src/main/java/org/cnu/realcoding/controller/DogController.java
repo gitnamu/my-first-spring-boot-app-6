5cd068cf4e791e21f2538ac9dd13a50f76c69797
HeoYooungJin98 <Youngjin.heo98@gmail.com>
2021-04-05 19:34:34 +0900

    Add getDogbyOwnerName getDogbyOwnerPhoneNumber getDogbyAllinf

--- src/main/java/org/cnu/realcoding/controller/DogController.java
+++ src/main/java/org/cnu/realcoding/controller/DogController.java
@@ -3,6 +3,7 @@ package org.cnu.realcoding.controller;
 import org.cnu.realcoding.domain.Dog;
 import org.cnu.realcoding.service.DogManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,17 +14,45 @@ public class DogController {
     @Autowired
     private DogManagementService dogManagementService;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/dogs")
-    public void createDogs(@RequestBody Dog dog) {
+    public void createDogs(@RequestBody Dog dog){
         dogManagementService.insertDog(dog);
     }
+    //add
+    public void getDogByOwnerName(@RequestBody Dog ownerName){
+        dogManagementService.getDogbyOwnerName(ownerName);
+    }
+    //add
+    public void getDogByOwnerPhoneNumber(@RequestBody Dog ownerPhoneNumber){
+        dogManagementService.getDogbyOwnerPhoneNumber(ownerPhoneNumber);
+    }
+    //add
+    public void getDogByAllinf(@RequestBody Dog name, Dog ownerName, Dog ownerPhoneNumber){
+        dogManagementService.getDogbyAllinf(name,ownerName,ownerPhoneNumber);
+    }
+    //add
+    public void modifyKind(@RequestBody Dog kind){
+        dogManagementService.modifyKind(kind);
+    }
+    //add
+    public void addRecord(@RequestBody Dog record){
+        dogManagementService.addRecord(record);
+    }
+    //add
+    public void modifyAll(@RequestBody Dog dog){
+
+    }
+
+
     @GetMapping("/dogs")
-    public List<Dog> getAllDogs() {
+    public List<Dog> getAllDogs(){
         return dogManagementService.getDogs();
     }
-    //localhost:8080/dogs/xxx
-    @GetMapping("/dogs{name}")
-        public Dog getDogByName(@PathVariable String name){
-        return dogManagementService.getDogByName(name);
-        }
+
+    //localhost:8080/dogs/[name]
+    @GetMapping("/dogs/{name}")
+    public void getDogByName(@PathVariable String name) {
+        dogManagementService.getDogByname(name);
+    }
 }
--- src/main/java/org/cnu/realcoding/service/DogManagementService.java
+++ src/main/java/org/cnu/realcoding/service/DogManagementService.java
@@ -10,21 +10,61 @@ import java.util.List;
 
 @Service
 public class DogManagementService {
+
     @Getter
-    private List<Dog> dogs = new ArrayList<>();
+    private List<Dog> dogs=new ArrayList<>();
 
     public void insertDog(Dog dog) {
-        dogs.add(dog);
+        for(Dog dog1 : dogs){
+            if(dogs.equals(dog.getName()) && dogs.equals(dog.getOwnerName()) && dogs.equals(dog.getOwnerPhoneNumber())){
+                //XXX http status return
+            }else{
+                dogs.add(dog);
+            }
+        }
     }
 
-    public Dog getDogByName(String name) {
 
+    public Dog getDogByname(String name) {
         for (Dog dog : dogs) {
             if (dog.getName().equals(name)) {
                 return dog;
             }
         }
-
         throw new DogNotFoundException();
     }
+    //add
+    public Dog getDogbyOwnerName(Dog ownerName){
+        for(Dog dog : dogs) {
+            if(dog.getOwnerName().equals(ownerName)) {
+                return dog;
+            }
+        }
+        throw new DogNotFoundException();
+    }
+    //add
+    public Dog getDogbyOwnerPhoneNumber(Dog ownerPhoneNumber) {
+        for(Dog dog : dogs){
+            if(dog.getOwnerPhoneNumber().equals(ownerPhoneNumber)) {
+                return dog;
+            }
+        }
+        throw new DogNotFoundException();
+    }
+    //add
+    public Dog getDogbyAllinf(Dog name, Dog ownerName, Dog ownerPhoneNumber) {
+        for(Dog dog : dogs){
+            if(dog.getName().equals(name) && dog.getOwnerName().equals(ownerName) && dog.getOwnerPhoneNumber().equals(ownerPhoneNumber)){
+                return dog;
+            }
+        }
+        throw new DogNotFoundException();
+    }
+    //add
+    public void modifyKind(Dog kind) {
+    }
+    //add
+    public void addRecord(Dog record) {
+    }
+
 }
