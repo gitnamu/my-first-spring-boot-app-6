314dbc4fa1ddacd9f065b25b76dd021ee57867b5
Beom Sik <ryuha_____@naver.com>
2021-04-06 21:04:49 +0900

    PatchDogByKind Update

--- src/main/java/org/cnu/realcoding/myfirstspringbootapp6/Service/DogManagementService.java
+++ src/main/java/org/cnu/realcoding/myfirstspringbootapp6/Service/DogManagementService.java
@@ -12,16 +12,66 @@ import java.util.List;
 public class DogManagementService {
 
     @Getter
-    private List<Dog> dogs=new ArrayList<>();
+    private List<Dog> dogs = new ArrayList<>();
 
     public void insertDog(Dog dog) {
-        dogs.add(dog);
+        //dogs.add(dog);
+        int token = 0;              //리스트안에 중복값이 없다면 토큰 0 있으면 1
+        if (dogs.size() == 0) {       //리스트가 비어있으면 바로 추가
+            dogs.add(dog);
+        } else {
+            for (Dog dog1 : dogs) { //중복값있는지 체크
+                if (dog1.getName().equals(dog.getName()) && dog1.getOwnerName().equals(dog.getOwnerName()) && dog1.getOwnerPhoneNumber().equals(dog.getOwnerPhoneNumber())) {
+                    //XXX http status return
+                    token = 1;
+                    break;
+                }
+            }
+            if (token == 0) dogs.add(dog);    //중복값 없으니 추가
+        }
     }
 
 
     public Dog getDogByname(String name) {
         for (Dog dog : dogs) {
             if (dog.getName().equals(name)) {
+                return dog;
+            }
+        }
+        throw new DogNotFoundException();
+    }
+
+    public Dog getDogByOwnerName(String ownerName) {
+        for (Dog dog : dogs) {
+            if (dog.getOwnerName().equals(ownerName)) {
+                return dog;
+            }
+        }
+        throw new DogNotFoundException();
+    }
+
+    public Dog getDogByOwnerPhoneNumber(String ownerPhoneNumber) {
+        for (Dog dog : dogs) {
+            if (dog.getOwnerPhoneNumber().equals(ownerPhoneNumber)) {
+                return dog;
+            }
+        }
+        throw new DogNotFoundException();
+    }
+
+    public Dog getDogByAllInf(String name, String ownerName, String ownerPhoneNumber) {
+        for (Dog dog : dogs) {
+            if (dog.getName().equals(name) && dog.getOwnerName().equals(ownerName) && dog.getOwnerPhoneNumber().equals(ownerPhoneNumber)) {
+                return dog;
+            }
+        }
+        throw new DogNotFoundException();
+    }
+
+    public Dog patchDogByKind(String name, String ownerName, String ownerPhoneNumber, String ChangeKind) {
+        for (Dog dog : dogs) {
+            if (dog.getName().equals(name) && dog.getOwnerName().equals(ownerName) && dog.getOwnerPhoneNumber().equals(ownerPhoneNumber)) {
+                dog.setKind(ChangeKind);
                 return dog;
             }
         }
--- src/main/java/org/cnu/realcoding/myfirstspringbootapp6/controller/DogController.java
+++ src/main/java/org/cnu/realcoding/myfirstspringbootapp6/controller/DogController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.acl.Owner;
 import java.util.List;
 
 @RestController
@@ -16,7 +17,7 @@ public class DogController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/dogs")
-    public void createDogs(@RequestBody Dog dog){
+    public void createDogs(@RequestBody Dog dog){   //신규 방문 강아지 등록
         dogManagementService.insertDog(dog);
     }
 
@@ -25,9 +26,21 @@ public class DogController {
         return dogManagementService.getDogs();
     }
 
+
     //localhost:8080/dogs/[name]
-    @GetMapping("/dogs/{name}")
-    public void getDogByName(@PathVariable String name) {
-        dogManagementService.getDogByname(name);
-    }
+    @GetMapping("/dogs/name/{name}")
+    public Dog getDogByName(@PathVariable String name) { return dogManagementService.getDogByname(name); }
+
+    @GetMapping("/dogs/ownerName/{OwnerName}")
+    public Dog getDogByOwnerName(@PathVariable String OwnerName) { return dogManagementService.getDogByOwnerName(OwnerName); }
+
+    @GetMapping("/dogs/OwnerPhoneNumber/{OwnerPhoneNumber}")
+    public Dog getDogByOwnerPhoneNumber(@PathVariable String OwnerPhoneNumber) { return dogManagementService.getDogByOwnerPhoneNumber(OwnerPhoneNumber); }
+
+    @GetMapping("/dogs/AllInf/{name},{OwnerName},{OwnerPhoneNumber}")
+    public Dog getDogByAllInf(@PathVariable String name, String OwnerName, String OwnerPhoneNumber) { return dogManagementService.getDogByAllInf(name, OwnerName, OwnerPhoneNumber); }
+
+    @PatchMapping("/dogs/AllInf/{name},{OwnerName},{OwnerPhoneNumber},{Kind}")
+    public Dog patchDogbyKind(@PathVariable String name, String OwnerName, String OwnerPhoneNumber, String ChangeKind, @RequestBody Dog patchDog) { return dogManagementService.patchDogByKind(name, OwnerName, OwnerPhoneNumber, ChangeKind); }
+
 }
