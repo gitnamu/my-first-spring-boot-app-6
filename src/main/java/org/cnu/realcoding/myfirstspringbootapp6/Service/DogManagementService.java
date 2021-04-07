69d7de18628379e75b1ec1fca330fa8b2a5ad762
Kim Namho <skagh1750@gmail.com>
2021-04-07 22:05:38 +0900

    fixed put method

--- src/main/java/org/cnu/realcoding/myfirstspringbootapp6/Service/DogManagementService.java
+++ src/main/java/org/cnu/realcoding/myfirstspringbootapp6/Service/DogManagementService.java
@@ -79,16 +79,18 @@ public class DogManagementService {
     }
 
     // 강아지 정보를 통째로 덮어쓰는 메소드
-    public void putDogAllInfo(String name, Dog newDog){
-        Dog originalDog = getDogByName(name);    // name의 이름을 가진 강아지 찾아서 originalDog에 저장
-        newDog.setMedicalRecords(originalDog.getMedicalRecords());  // 이전 정보의 진료기록 보존
-        dogs.remove(originalDog);    // 원래 정보 삭제
-        dogs.add(newDog);   // 변경된 정보 리스트에 추가
+    public void putDogAllInfo(String name, String ownerName, String ownerPhoneNumber, Dog newDog) {
+        Dog originalDog = getDogByAllInf(name,ownerName,ownerPhoneNumber);    // name의 이름을 가진 강아지 찾아서 originalDog에 저장
+        // 입력받은 정보 옮기기 (medical records는 제외하고)
+        originalDog.setName(newDog.getName());
+        originalDog.setKind(newDog.getKind());
+        originalDog.setOwnerName(newDog.getOwnerName());
+        originalDog.setOwnerPhoneNumber(newDog.getOwnerPhoneNumber());
     }
 
     // 진료기록 추가 메소드
-    public void patchDogMedicalRecords(String name, String newRecord){
-        Dog dog = getDogByName(name);   // name으로 강아지 찾아서 dog에 저장
+    public void patchDogMedicalRecords(String name, String ownerName, String ownerPhoneNumber, String newRecord) {
+        Dog dog = getDogByAllInf(name,ownerName,ownerPhoneNumber);   // name으로 강아지 찾아서 dog에 저장
         dog.getMedicalRecords().add(newRecord); // dog의 진료기록 리스트에 새로운 진료기록 추가
     }
 }
