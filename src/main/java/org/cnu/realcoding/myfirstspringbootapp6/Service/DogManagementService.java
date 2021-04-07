4f41556b798bedfa443daa5e91f3d97af58a50c8
rlawogns0411 <rlawogns0411@naver.com>
2021-04-08 01:16:42 +0900

    mongodb api 1~5수정완료

--- src/main/java/org/cnu/realcoding/myfirstspringbootapp6/Service/DogManagementService.java
+++ src/main/java/org/cnu/realcoding/myfirstspringbootapp6/Service/DogManagementService.java
@@ -3,6 +3,9 @@ package org.cnu.realcoding.myfirstspringbootapp6.Service;
 import lombok.Getter;
 import org.cnu.realcoding.myfirstspringbootapp6.domain.Dog;
 import org.cnu.realcoding.myfirstspringbootapp6.exception.DogNotFoundException;
+import org.cnu.realcoding.myfirstspringbootapp6.exception.ExistingDogException;
+import org.cnu.realcoding.myfirstspringbootapp6.repository.DogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,63 +14,78 @@ import java.util.List;
 @Service
 public class DogManagementService {
 
-    @Getter
-    private List<Dog> dogs=new ArrayList<>();
+    @Autowired
+    private DogRepository dogRepository;
 
-    public void insertDog(Dog dog) {
-        //dogs.add(dog);
-        int token = 0;              //리스트안에 중복값이 없다면 토큰 0 있으면 1
-        if (dogs.size()==0) {       //리스트가 비어있으면 바로 추가
-            dogs.add(dog);
-        } else {
-            for (Dog dog1 : dogs) { //중복값있는지 체크
-                if (dog1.getName().equals(dog.getName()) && dog1.getOwnerName().equals(dog.getOwnerName()) && dog1.getOwnerPhoneNumber().equals(dog.getOwnerPhoneNumber())) {
-                    //XXX http status return
-                    token=1;
-                    break;
-                }
+
+    public void insertDog(Dog dog) {    //값 입력
+        Dog fdog=dogRepository.findDogByAllInf(dog.getName(),dog.getOwnerName(),dog.getOwnerPhoneNumber());
+        if (fdog==null) dogRepository.insertDog(dog);
+        else {
+            if (fdog.getName().equals(dog.getName()) &&
+                    fdog.getName().equals(dog.getName()) &&
+                    fdog.getName().equals(dog.getName())) {
+                throw new ExistingDogException();
             }
-            if (token==0) dogs.add(dog);    //중복값 없으니 추가
+            else{
+                dogRepository.insertDog(dog);
+            }
         }
     }
+    public List<Dog> getAllDogs() { //개 전체 조회
+        List<Dog> dog = dogRepository.findAllDog();
+        if (dog.isEmpty()) throw new DogNotFoundException();
+        return dog;
+    }
 
+    public List<Dog> getDogByName(String name) {    //개이름으로 조회
+        List<Dog> dog = dogRepository.findDogByName(name);
+        if (dog.isEmpty()) throw new DogNotFoundException();
+        return dog;
+    }
 
-    public List<Dog> getDogByname(String name) {
-        List<Dog> getdogs=new ArrayList<>();
+    public List<Dog> getDogByOwnerName(String ownerName) {  //주인이름으로 조회
+        List<Dog> dog = dogRepository.findDogByOwnerName(ownerName);
+        if (dog.isEmpty()) throw new DogNotFoundException();
+        return dog;
+    }
+
+    public List<Dog> getDogByOwnerPhoneNumber(String ownerPhoneNumber) {    //폰번호로 조회
+        List<Dog> dog = dogRepository.findDogByOwnerPhoneNumber(ownerPhoneNumber);
+        if (dog.isEmpty()) throw new DogNotFoundException();
+        return dog;
+    }
+
+    public Dog getDogByAllInf(String name, String ownerName, String ownerPhoneNumber) { //모든 정보로 조회
+        Dog dog = dogRepository.findDogByAllInf(name,ownerName,ownerPhoneNumber);
+        if (dog==null) throw new DogNotFoundException();
+        return dog;
+    }
+/*
+    public Dog patchDogByKind(String name, String ownerName, String ownerPhoneNumber, String ChangeKind) {
         for (Dog dog : dogs) {
-            if (dog.getName().equals(name)) {
-                getdogs.add(dog);
-            }
-        }
-        if (getdogs.size()==0) throw new DogNotFoundException();
-        return getdogs;
-    }
-    public List<Dog> getDogByOwnerName(String ownerName){
-        List<Dog> getdogs=new ArrayList<>();
-        for(Dog dog : dogs) {
-            if(dog.getOwnerName().equals(ownerName)) {
-                getdogs.add(dog);
-            }
-        }
-        if (getdogs.size()==0) throw new DogNotFoundException();
-        return getdogs;
-    }
-    public List<Dog> getDogByOwnerPhoneNumber(String ownerPhoneNumber) {
-        List<Dog> getdogs=new ArrayList<>();
-        for(Dog dog : dogs){
-            if(dog.getOwnerPhoneNumber().equals(ownerPhoneNumber)) {
-                getdogs.add(dog);
-            }
-        }
-        if (getdogs.size()==0) throw new DogNotFoundException();
-        return getdogs;
-    }
-    public Dog getDogByAllInf(String name, String ownerName, String ownerPhoneNumber) {
-        for(Dog dog : dogs){
-            if(dog.getName().equals(name) && dog.getOwnerName().equals(ownerName) && dog.getOwnerPhoneNumber().equals(ownerPhoneNumber)){
+            if (dog.getName().equals(name) && dog.getOwnerName().equals(ownerName) && dog.getOwnerPhoneNumber().equals(ownerPhoneNumber)) {
+                dog.setKind(ChangeKind);
                 return dog;
             }
         }
         throw new DogNotFoundException();
     }
+
+    // 강아지 정보를 통째로 덮어쓰는 메소드
+    public void putDogAllInfo(String name, Dog newDog){
+        Dog originalDog = getDogByName(name);    // name의 이름을 가진 강아지 찾아서 originalDog에 저장
+        newDog.setMedicalRecords(originalDog.getMedicalRecords());  // 이전 정보의 진료기록 보존
+        dogs.remove(originalDog);    // 원래 정보 삭제
+        dogs.add(newDog);   // 변경된 정보 리스트에 추가
+    }
+
+    // 진료기록 추가 메소드
+    public void patchDogMedicalRecords(String name, String newRecord){
+        Dog dog = getDogByName(name);   // name으로 강아지 찾아서 dog에 저장
+        dog.getMedicalRecords().add(newRecord); // dog의 진료기록 리스트에 새로운 진료기록 추가
+
+    }*/
+
+
 }
